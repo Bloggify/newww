fd0e7e222e5f1ffa7417da5101ebeb9fca768bf3
Ionică Bizău <bizauionica@gmail.com>
2016-09-15 13:45:23 +0300

    Production Fixes

--- bloggify/plugin-configs/bloggify-plugin-manager.js
+++ bloggify/plugin-configs/bloggify-plugin-manager.js
@@ -2,9 +2,10 @@ module.exports = {
     plugins: [
         "bloggify-router",
         "bloggify-ajs-renderer",
-        //"bloggify-analytics",
-        //"bloggify-mongoose",
-        //"bloggify-template-renderer",
         "bloggify-viewer"
     ]
 };
+
+if (process.env.NODE_ENV === "production") {
+    module.exports.plugins.push("bloggify-analytics", "bloggify-mongoose");
+}
